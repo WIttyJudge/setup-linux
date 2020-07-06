04057169750e651e4874dab0bd65dd55beafa8c4
Ruslan <kros8925@gmail.com>
2020-07-06 20:36:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 # This is a comment!
 echo Hello From Installer
+
+function installing_package {
+  echo "--- installing package: $1"
+}
+
+installing_package privet
+installing_package kak dela
