7a57686b7a33249c8e0bf6e2deaba389226534bc
Ruslan <kros8925@gmail.com>
2020-07-07 14:40:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-# This is a comment!
+
+if [ "$(whoami)" != "root"]
+then 
+  echo "You have to run this script as Superuser!"
+  exit 1
+fi
+
 echo Hello From Installer
 
 install_package()
 {
   echo "--- Installing package: $1"
+  sudo apt 
 }
+
+sudo apt-get update
 
 install_package privet
 install_package kakdela
