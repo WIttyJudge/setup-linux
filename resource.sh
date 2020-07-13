bcbe7d290d7483e188543769d0107dcb55b66643
Ruslan <kros8925@gmail.com>
2020-07-13 13:44:24 +0300

    okay

---
+++ resource.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+install_resource() {
+  # $1 - name of the installed file. $2 - resource to download
+  wget -O ~/.dotfiles/$1 "$2"
+}
+
+install_resource discord.deb https://discord.com/api/download?platform=linux&format=deb
+dpkg -i discord.deb
