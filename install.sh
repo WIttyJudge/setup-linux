1fe4de306558e3c19d2e33e1efa888266d8120af
Ruslan <kros8925@gmail.com>
2020-07-13 17:52:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ fi
 # Some of the style and formats for the output
 echo "\033[38;5;16m \033[48;5;174m**** HELLO FROM INSTALLER FOR UBUNTU ****\n\033[0m"
 
-dir=$HOME/.dotfiles
-if [ ! -d "$dir" ]; then
-  git clone -b Ubuntu https://github.com/WIttyJudge/dotfiles $dir
-fi
+# dir=$HOME/.dotfiles
+# if [ ! -d "$dir" ]; then
+#   git clone -b Ubuntu https://github.com/WIttyJudge/dotfiles $dir
+# fi
 
 # Setup the configuration for speed up system
-sh "$dir/setup.sh"
+sh ./setup.sh
 
 # Install a packages I used by apt package manager
-sh "$dir/package.sh"
+sh ./package.sh
 
 # Install programs I used from another resource.
-sh "$dir/resource.sh"
+sh ./resource.sh
