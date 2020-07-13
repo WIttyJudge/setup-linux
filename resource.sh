bd10aca905a2bbb352d630df87359ead649bca03
Ruslan <kros8925@gmail.com>
2020-07-13 23:13:53 +0300

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -7,6 +7,3 @@ install_resource() {
 
 install_resource discord.deb https://discord.com/api/download?platform=linux&format=deb
 dpkg -i discord.deb
-
-install_resource virtualbox https://download.virtualbox.org/virtualbox/6.1.10/virtualbox-6.1_6.1.10-138449~Ubuntu~bionic_amd64.deb
-apt install ./virtualbox
