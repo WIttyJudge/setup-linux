0a581faa11cf15ec2e42a3f98c53dd0ff6e14534
Ruslan <kros8925@gmail.com>
2020-07-14 00:00:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Running the script only by root
-if [[ "$(whoami)" != "root" ]]; then
+if [ "$(whoami)" != "root" ]; then
   echo "You have to run this script with superuser"
   exit 1
 fi
