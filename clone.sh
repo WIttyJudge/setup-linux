ef3a607468e1d271be83bf5880cd802a18d564a5
Ruslan <kros8925@gmail.com>
2020-07-13 18:36:15 +0300

    asdasas

--- clone.sh
+++ clone.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo apt install -y git
+# Running the script only by root
+if [ "$(whoami)" != "root" ]; then
+  echo "You have to run this script with superuser"
+  exit 1
+fi
+
+apt install -y git
 
 dir=$HOME/.dotfiles
 if [ ! -d "$dir" ]; then 
   git clone -b Ubuntu https://github.com/WittyJudge/dotfiles $dir
 fi
+
+chown -R $(whoami) $dir/.dotfiles
--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Running the script only by root
-if [ "$(whoami)" != "root" ]
-then
+if [ "$(whoami)" != "root" ]; then
   echo "You have to run this script with superuser"
   exit 1
 fi
