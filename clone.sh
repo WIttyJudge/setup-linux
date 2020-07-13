9d8118973a80b86587dcd5ede6342f73adbb7f99
Ruslan <kros8925@gmail.com>
2020-07-13 23:51:47 +0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -14,7 +14,7 @@ dir=$HOME/.dotfiles
 if [ ! -d "$dir" ]; then 
   echo -e "\033[38;5;28m---- Clonning repository\033[0m\n"
   
-  git clone -b Ubuntu https://github.com/WittyJudge/dotfiles $dir
+  git clone -b Ubuntu https://github.com/WittyJudge/dotfiles $dir && chown -R $(echo $USERNAME) $dir
 fi
 
-chown -R $(echo $USERNAME) $dir
+
