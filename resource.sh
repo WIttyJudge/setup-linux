b7c707584de21b4cc9e5a574884bae2bcfe908c5
Ruslan <kros8925@gmail.com>
2020-07-17 21:01:47 +0300

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -7,7 +7,7 @@ fi
 
 install_resource() {
   # $1 - name of the installed file. $2 - resource to download
-  wget -O $HOME/.dotfiles/$1 "$2"
+  wget -O $HOME/.dotfiles/$resource_folder/$1 "$2"
 }
 
 install_resource discord.deb https://discordapp.com/api/download?platform=linux&format=deb
