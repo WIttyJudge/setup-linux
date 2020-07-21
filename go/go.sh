a8b4366ee9efcda20858799fe203d2149e109908
Ruslan <kros8925@gmail.com>
2020-07-21 20:41:09 +0300

    Update go.sh

--- go/go.sh
+++ go/go.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-go_folder=go1.14.6
+go_folder=go1.14.6.linux-amd64.tar.gz
 
 echo "Installing Golang"
-wget https://github.com/golang/go/archive/$go_folder.tar.gz
-sudo tar -xvf $go_folder.tar.gz
-sudo mv go-$go_folder /usr/local/go
+wget https://golang.org/dl/$go_folder
+sudo tar -C /usr/local -xzf $go_folder
 
 # export GOROOT=/usr/local/go
 # export GOPATH=$HOME/golib
@@ -15,10 +14,16 @@ sudo mkdir -p $HOME/code/src/github.com/wittyjudge
 sudo mkdir -p $HOME/golib
 
 cat <<EOF >> ~/.profile
+# root
 export GOROOT=/usr/local/go
-export GOPATH=$HOME/golib
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-export GOPATH=$GOPATH:$HOME/code
+
+# libs from golang
+export GOPATH=$HOME/golib
+export PATH=$PATH:$GOPATH/bin
+
+# my folder
+export GOPATH=$GOPATH:/home/witty/code
 EOF
 
 source ~/.profile
