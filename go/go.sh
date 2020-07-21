b2be55f9393be3fa103a0ae3d6b8910100e9649f
Ruslan <kros8925@gmail.com>
2020-07-21 20:12:00 +0300

    Update go.sh

--- go/go.sh
+++ go/go.sh
@@ -5,7 +5,7 @@ go_folder=go1.14.6
 echo "Installing Golang"
 wget https://github.com/golang/go/archive/$go_folder.tar.gz
 sudo tar -xvf $go_folder.tar.gz
-sudo mv $go_folder /usr/local/go
+sudo mv go-$go_folder /usr/local/go
 
 # export GOROOT=/usr/local/go
 # export GOPATH=$HOME/golib
