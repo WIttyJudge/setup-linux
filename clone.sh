6855a8eb2247f81f647591955701755c0120e9fa
Ruslan <kros8925@gmail.com>
2020-07-26 20:15:51 +0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -10,11 +10,11 @@ echo "\033[38;5;16m \033[48;5;174m**** CLONNING REPOSITORY FOR UBUNTU ****\n\033
 
 apt install -y git
 
-dir=$HOME/.dotfiles
+dir=$HOME/setup-linux
 if [ ! -d "$dir" ]; then 
   echo -e "\033[38;5;28m---- Clonning repository\033[0m\n"
   
-  git clone -b Ubuntu https://github.com/WittyJudge/dotfiles $dir
+  git clone -b Ubuntu https://github.com/WittyJudge/setup-linux $dir
 fi
 
 user=$(who | cut -d ' ' -f1)
