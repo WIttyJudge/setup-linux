a2d437504f3e9c4c1540aff79a739c9bfb3f8674
Ruslan <kros8925@gmail.com>
2020-08-02 01:53:55 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -19,7 +19,9 @@ install_package terminator
 install_package ripgrep
 install_package httpie
 install_package chromium-browser
+install_package neofetch
 
+# Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 install_package yarn
